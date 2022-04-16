2a9773fac4447aa1f0fa3c2e4f44902932d28bc3
im-adnan <sayedmuhammadadnan@gmail.com>
2022-04-16 13:21:40 +0530

    added app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,98 @@
-import 'package:calender/calanderPage.dart';
-import 'package:calender/eventPage.dart';
+import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+import 'model/event.dart';
+import 'pages/mobile/mobile_home_page.dart';
+import 'pages/web/web_home_page.dart';
+import 'widgets/responsive_widget.dart';
+
+DateTime get _now => DateTime.now();
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      routes: {
-        '/': (context) => calendarPage(),
-        '/event': (context) => eventPage(),
-      },
+    return CalendarControllerProvider<Event>(
+      controller: EventController<Event>()..addAll(_events),
+      child: MaterialApp(
+        title: 'Flutter Calendar Page Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        home: MobileHomePage(),
+      ),
     );
   }
 }
+
+List<CalendarEventData<Event>> _events = [
+  CalendarEventData(
+    date: _now,
+    event: Event(title: "Joe's Birthday"),
+    title: "Project meeting",
+    description: "Today is project meeting.",
+    startTime: DateTime(_now.year, _now.month, _now.day, 18, 30),
+    endTime: DateTime(_now.year, _now.month, _now.day, 22),
+  ),
+  CalendarEventData(
+    date: _now.add(Duration(days: 1)),
+    startTime: DateTime(_now.year, _now.month, _now.day, 18),
+    endTime: DateTime(_now.year, _now.month, _now.day, 19),
+    event: Event(title: "Wedding anniversary"),
+    title: "Wedding anniversary",
+    description: "Attend uncle's wedding anniversary.",
+  ),
+  CalendarEventData(
+    date: _now,
+    startTime: DateTime(_now.year, _now.month, _now.day, 14),
+    endTime: DateTime(_now.year, _now.month, _now.day, 17),
+    event: Event(title: "Football Tournament"),
+    title: "Football Tournament",
+    description: "Go to football tournament.",
+  ),
+  CalendarEventData(
+    date: _now.add(Duration(days: 3)),
+    startTime: DateTime(_now.add(Duration(days: 3)).year,
+        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 10),
+    endTime: DateTime(_now.add(Duration(days: 3)).year,
+        _now.add(Duration(days: 3)).month, _now.add(Duration(days: 3)).day, 14),
+    event: Event(title: "Sprint Meeting."),
+    title: "Sprint Meeting.",
+    description: "Last day of project submission for last year.",
+  ),
+  CalendarEventData(
+    date: _now.subtract(Duration(days: 2)),
+    startTime: DateTime(
+        _now.subtract(Duration(days: 2)).year,
+        _now.subtract(Duration(days: 2)).month,
+        _now.subtract(Duration(days: 2)).day,
+        14),
+    endTime: DateTime(
+        _now.subtract(Duration(days: 2)).year,
+        _now.subtract(Duration(days: 2)).month,
+        _now.subtract(Duration(days: 2)).day,
+        16),
+    event: Event(title: "Team Meeting"),
+    title: "Team Meeting",
+    description: "Team Meeting",
+  ),
+  CalendarEventData(
+    date: _now.subtract(Duration(days: 2)),
+    startTime: DateTime(
+        _now.subtract(Duration(days: 2)).year,
+        _now.subtract(Duration(days: 2)).month,
+        _now.subtract(Duration(days: 2)).day,
+        10),
+    endTime: DateTime(
+        _now.subtract(Duration(days: 2)).year,
+        _now.subtract(Duration(days: 2)).month,
+        _now.subtract(Duration(days: 2)).day,
+        12),
+    event: Event(title: "Chemistry Viva"),
+    title: "Chemistry Viva",
+    description: "Today is Joe's birthday.",
+  ),
+];
