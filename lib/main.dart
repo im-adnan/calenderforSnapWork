9599ae532a74ebc1ab497816c055aede2526e1ba
im-adnan <sayedmuhammadadnan@gmail.com>
2022-04-16 12:13:22 +0530

    original

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calender/calanderPage.dart';
+import 'package:calender/eventPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      routes: {
+        '/': (context) => calendarPage(),
+        '/event': (context) => eventPage(),
+      },
+    );
+  }
+}
